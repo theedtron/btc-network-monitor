0c37a171562ba81abc41181c3405ceb5dd81b57c
yemmyharry <yemmyharry@gmail.com>
2024-03-16 12:08:06 +0100

    add migration function

---
+++ internal/migration/migration.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"btc-network-monitor/internal/core/domain"
+	"btc-network-monitor/internal/database"
+)
+
+func Migrate() {
+	db := database.ConnectDB()
+	db.AutoMigrate(&domain.User{})
+}
+
+func main() {
+	Migrate()
+}
