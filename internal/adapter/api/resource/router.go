7af6f32870a03527fb7681d7d41e14e44c1c892d
yemmyharry <yemmyharry@gmail.com>
2024-03-16 12:09:30 +0100

    add router

---
+++ internal/adapter/api/resource/router.go
@@ -0,0 +1,27 @@
+package resource
+
+import (
+	"btc-network-monitor/internal/adapter/api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *HTTPHandler) Routes(router *gin.Engine) {
+	router.Use(middleware.LoggingMiddleWare())
+	api := router.Group("/api/v1")
+
+	api.GET("/healthcheck", s.HealthCheck)
+	api.POST("/register", s.Register)
+	api.POST("/login", s.Login)
+
+	//user routers
+	users := api.Group("/users")
+	{
+		users.Use(middleware.AuthMiddleware())
+		users.GET("/", s.GetAllUsers)
+		users.PUT("/:id", s.UpdateUser)
+		users.GET("/:id", s.FindUser)
+	}
+
+	router.NoRoute(func(c *gin.Context) { c.String(404, "Not found") })
+}
