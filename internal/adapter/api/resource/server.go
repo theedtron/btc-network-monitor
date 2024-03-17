e870698590eb074bcb09277204c13149dc6620b5
yemmyharry <yemmyharry@gmail.com>
2024-03-17 22:37:52 +0100

    add monitor service

--- internal/adapter/api/resource/server.go
+++ internal/adapter/api/resource/server.go
@@ -3,14 +3,16 @@ package resource
 import "btc-network-monitor/internal/core/services"
 
 type HTTPHandler struct {
-	userService *services.UserService
-	authService *services.AuthService
+	userService    *services.UserService
+	authService    *services.AuthService
+	monitorService *services.MonitorService
 }
 
 func NewHTTPHandler(options ...interface{}) *HTTPHandler {
 	handler := &HTTPHandler{
-		userService: services.NewUserService(),
-		authService: services.NewAuthService(),
+		userService:    services.NewUserService(),
+		authService:    services.NewAuthService(),
+		monitorService: services.NewMonitorService(),
 	}
 	return handler
 }
