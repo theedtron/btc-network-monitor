1484d5ce228776bb39fbb64b27b55cfa63247ed1
yemmyharry <yemmyharry@gmail.com>
2024-03-24 22:41:53 +0100

    fix: refactor code

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -14,13 +14,30 @@ import (
 )
 
 type EmailData struct {
-	FirstName string
-	Subject string
-	MailTo string
+	FirstName     string
+	Subject       string
+	MailTo        string
 	Confirmations int64
-	TxId string
+	TxId          string
 }
 
+type NotificationConfig struct {
+	from       string
+	smtpPass   string
+	smtpUser   string
+	smtpHost   string
+	portString string
+}
+
+func NewNotificationConfig() NotificationConfig {
+	return NotificationConfig{
+		from:       os.Getenv("EMAIL_FROM"),
+		smtpPass:   os.Getenv("SMTP_PASS"),
+		smtpUser:   os.Getenv("SMTP_USER"),
+		smtpHost:   os.Getenv("SMTP_HOST"),
+		portString: os.Getenv("SMTP_PORT"),
+	}
+}
 
 // 👇 Email template parser
 func ParseTemplateDir(dir string) (*template.Template, error) {
@@ -44,38 +61,32 @@ func ParseTemplateDir(dir string) (*template.Template, error) {
 	return template.ParseFiles(paths...)
 }
 
-// ? Email template parser
-func SendEmail(data *EmailData) error {
-	// Sender data.
-	from := os.Getenv("EMAIL_FROM")
-	smtpPass := os.Getenv("SMTP_PASS")
-	smtpUser := os.Getenv("SMTP_USER")
-	smtpHost := os.Getenv("SMTP_HOST")
-	portString := os.Getenv("SMTP_PORT")
+func (s *NotificationConfig) SendEmail(data *EmailData) error {
 
-	smtpPort, err := strconv.Atoi(portString)
+	smtpPort, err := strconv.Atoi(s.portString)
 	if err != nil {
-        // ... handle error
-		logger.Error("Could not covert port string" + err.Error())
-    }
+		logger.Error("Could not convert port string" + err.Error())
+		return err
+	}
 
 	var body bytes.Buffer
 	template, err := ParseTemplateDir("internal/views/templates")
 	if err != nil {
 		logger.Error("Could not parse template" + err.Error())
+		return err
 	}
 
 	template.ExecuteTemplate(&body, "notification.html", &data)
 
 	m := gomail.NewMessage()
 
-	m.SetHeader("From", from)
+	m.SetHeader("From", s.from)
 	m.SetHeader("To", data.MailTo)
 	m.SetHeader("Subject", data.Subject)
 	m.SetBody("text/html", body.String())
 	m.AddAlternative("text/plain", html2text.HTML2Text(body.String()))
 
-	d := gomail.NewDialer(smtpHost, smtpPort, smtpUser, smtpPass)
+	d := gomail.NewDialer(s.smtpHost, smtpPort, s.smtpUser, s.smtpPass)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	logger.Info(d)
 
